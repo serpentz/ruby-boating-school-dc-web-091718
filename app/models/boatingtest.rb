f14d3b2f36fb0322c341ed1bacb96dd11294a57d
serpentz <abyicherinet@gmail.com>
2018-09-21 12:53:53 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,13 @@
 class BoatingTest
 
+	@@all = []
+	def initialize(student, test_name, instructor)
+		@student, @test_name, @instructor = student, test_name, instructor
+		@@all << self
+	end
+
+	def all
+		@all
+	end
+	
 end
---
+++ app/run.rb
@@ -0,0 +1,4 @@
+require_relative "models/boatingtest.rb"
+require_relative "models/instuctor.rb"
+require_relative "models/student.rb"
+
